f6e45d0ed80a91756fc37bfe164e7c00256baee5
zhangu-08 <kshan.coder@outlook.com>
2026-08-23 17:35:26 -0400

    chore: add shadcn library

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import geistLatin from '@fontsource-variable/geist/files/geist-latin-wght-normal.woff2?url';
 
 	import './layout.css';
 
@@ -8,6 +9,13 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link
+		rel="preload"
+		href={geistLatin}
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
 <div class="m-4">
 	<h1>Welcome to SvelteKit</h1>
 	<p>
@@ -5,4 +9,7 @@
 		<a class="text-blue-800 underline" href="https://svelte.dev/docs/kit"> svelte.dev/docs/kit </a>
 		to read the documentation
 	</p>
+
+	<Button variant="secondary">Cancel</Button>
+	<Button variant="default">Save Threshold</Button>
 </div>
